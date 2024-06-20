6e21bac344b7e31d13c4e7e0231121a524b16505
Sauce <786314327@qq.com>
2024-06-20 19:36:02 +0800

    Create colorscheme.lua

---
+++ lua/plugins/colorscheme.lua
@@ -0,0 +1,13 @@
+return {
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme =
+      -- "nord",
+      -- "catppuccin",
+      "tokyonight",
+    },
+  },
+
+  { "shaunsingh/nord.nvim" },
+}
