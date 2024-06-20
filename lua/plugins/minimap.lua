c2c4ed30e332f4532828e135613892637bd8ac53
Sauce <786314327@qq.com>
2024-06-20 19:34:48 +0800

    Update minimap.lua

--- lua/plugins/minimap.lua
+++ lua/plugins/minimap.lua
@@ -1,21 +1,35 @@
 return {
-  "wfxr/minimap.vim",
-  --   build = "cargo install --locked code-minimap",
-  --   lazy = false,
-  --   cmd = { "Minimap", "MinimapClose", "MinimapToggle", "MinimapRefresh", "MinimapUpdateHighlight" },
-  --   init = function()
-  --     vim.cmd("let g:minimap_width = 10")
-  --     vim.cmd("let g:minimap_auto_start = 1")
-  --     vim.cmd("let g:minimap_auto_start_win_enter = 1")
-  --   end,
+  "echasnovski/mini.map",
+  version = false,
   keys = {
-    { "<leader>m", "<cmd>MinimapToggle<cr>", desc = "Toggle Minimap", mode = { "n" } },
+    { "<leader>mc", function() require("mini.map").close() end,        desc = "Close mini.map" },
+    { "<leader>mf", function() require("mini.map").toggle_focus() end, desc = "Toggle focus of mini.map" },
+    { "<leader>mo", function() require("mini.map").open() end,         desc = "Open mini.map" },
+    { "<leader>mr", function() require("mini.map").refresh() end,      desc = "Refresh mini.map with symbols" },
+    { "<leader>ms", function() require("mini.map").toggle_side() end,  desc = "Toggle side of mini.map" },
+    { "<leader>mt", function() require("mini.map").toggle() end,       desc = "Toggle mini.map" },
   },
-  --   config = function()
-  --     vim.cmd("let g:minimap_highlight_range = 1")
-  --     vim.cmd("let g:minimap_highlight_search = 1")
-  --     --     vim.cmd("let g:minimap_width = 10")
-  --     -- vim.cmd("let g:minimap_auto_start = 0")
-  --     --     vim.cmd("let g:minimap_auto_start_win_enter = 1")
-  --   end,
+  opts = function(_, opts)
+    local map = require("mini.map")
+    opts.integrations = {
+      -- map.gen_integration.diff({
+      --   add = "MiniDiffSignAdd",
+      --   change = "MiniDiffSignChange",
+      --   delete = "MiniDiffSignDelete"
+      -- }),
+      map.gen_integration.diagnostic({
+        error = "DiagnosticFloatingError",
+        warn = "DiagnosticFloatingWarn",
+        info = "DiagnosticFloatingInfo",
+        hint = "DiagnosticFloatingHint",
+      }),
+      map.gen_integration.builtin_search(),
+      map.gen_integration.diff(),
+      -- map.gen_integration.diagnostic(),
+    }
+    opts.symbols = {
+      scroll_line = '鈻?,
+      encode = map.gen_encode_symbols.dot("4x2"),
+    }
+  end,
 }
